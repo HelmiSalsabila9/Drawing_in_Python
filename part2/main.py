bf3d42a24f2868c66fd1cf7ff34671de4e6b0e0c
HelmiSalsabila9 <helmisalsabila9@gmail.com>
2022-11-02 20:04:08 +0700

    part2

--- part2/main.py
+++ part2/main.py
@@ -1,2 +1,32 @@
 
 
+# SKUYMI [helmisalsabila.com]
+
+import turtle as t 
+import colorsys as c 
+
+t.bgcolor('black')
+t.tracer(100)
+
+h = 0
+
+def gambar(asli, n):
+    t.circle(50+n, 30)
+    t.left(asli)
+    t.circle(50+n, 60)
+
+t.pensize(4)
+t.goto(-20, 0)
+
+for mi in range(500):
+    warna = c.hsv_to_rgb(h,1,1)
+    h += 0.1
+    t.pencolor(warna)
+    gambar(90, mi)
+    gambar(160, mi)
+    t.penup()
+    gambar(180, mi)
+    gambar(90, mi)
+    t.down()
+
+t.done()
